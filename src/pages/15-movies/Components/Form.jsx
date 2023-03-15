6497b8eade94e96192b3f00150908f0eaa6fa64a
anderecc <andersondbl06@gmail.com>
2023-03-15 13:03:46 -0300

    add preventDefault

--- src/pages/15-movies/Components/Form.jsx
+++ src/pages/15-movies/Components/Form.jsx
@@ -5,7 +5,7 @@ import styles from '../Styles/form.module.sass';
 let Form = () => {
     let { state, handleSetSearch } = useContext(AppContext);
     return (
-        <form className={styles.container}>
+        <form className={styles.container} onSubmit={(e) => e.preventDefault()}>
             <label>Search a movie.</label>
             <input
                 type="text"
